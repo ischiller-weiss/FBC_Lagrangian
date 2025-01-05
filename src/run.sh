28fb8389df0497b17066f9c86257639133060bdc
Ilana <ischiller-weiss@geomar.de>
2025-01-05 10:20:45 +0100

    nesh specific resources

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=parcels
-#SBATCH --output=../logs/parcels.out
-#SBATCH --error=../logs/parcels.err
-#SBATCH --time=10-00:00:00
+#SBATCH --output=../logs/parcels_%j.out
+#SBATCH --error=../logs/parcels_%j.err
+#SBATCH --time=9-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=200G
+#SBATCH --mem=250G
+#SBATCH --partition=base
+#SBATCH --qos=long
 
 source activate ~/miniconda3/envs/py3_std_maps_2023-11-20
 python experiment.py
