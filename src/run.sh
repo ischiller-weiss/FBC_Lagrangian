bbfcc59e83078e467901e09209dfe25482c80e7c
Ilana <123480345+ischiller-weiss@users.noreply.github.com>
2026-02-16 16:23:37 +0100

    run 1994 - 2010, missing files

--- src/run.sh
+++ src/run.sh
@@ -11,4 +11,4 @@
 source ~/.bashrc
 conda activate ~/miniconda3/envs/py3_std_maps_2023-11-20
 echo "Running parcels"
-python experiment.py --release_start 2011-01-01T00:00:00 --release_end 2015-12-31T00:00:00 --seeding uniform --frequency 5D --output_dir ../data/uniform_release_clim_land_nans/
+python experiment.py --release_start 1994-02-01T00:00:00 --release_end 2010-12-31T00:00:00 --seeding uniform --frequency 5D --output_dir ../data/uniform_release_clim_land_nans/
