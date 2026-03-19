f0a7efbafb0ef635e71ce5ec68e5be1173f70675
Ilana <123480345+ischiller-weiss@users.noreply.github.com>
2026-03-19 08:28:28 +0100

    running 2016 - 2020

--- src/run.sh
+++ src/run.sh
@@ -6,9 +6,8 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=30G
 #SBATCH --partition=data
-#SBATCH --exclude=nesh-dm01
 
 source ~/.bashrc
 conda activate ~/miniconda3/envs/py3_std_maps_2023-11-20
 echo "Running parcels"
-python experiment.py --release_start 1994-02-01T00:00:00 --release_end 2010-12-31T00:00:00 --seeding uniform --frequency 5D --output_dir ../data/uniform_release_clim_land_nans/
+python experiment.py --release_start 2016-01-01T00:00:00 --release_end 2020-12-31T00:00:00 --seeding uniform --frequency 5D --output_dir ../data/uniform_release_clim_land_nans/
